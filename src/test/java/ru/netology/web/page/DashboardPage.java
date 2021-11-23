afa5468e663b06509be0d4251ad4f2c437759620
Korotkov Dmitry <2110821@mail.ru>
2021-11-23 16:15:17 +0300

    check of replenishment was removed in tests,data class cardsBalance was deleted

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,20 +1,15 @@
 package ru.netology.web.page;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import ru.netology.web.data.DataHelper;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
     private SelenideElement heading = $("[data-test-id=dashboard]");
-    private SelenideElement firstCard = $("[data-test-id=\"92df3f1c-a033-48e6-8390-206f6b1f56c0\"]");
-    private SelenideElement secondCard = $("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"]");
-    private ElementsCollection cards = $$ (".list__item");
+    private ElementsCollection cards = $$(".list__item");
     private SelenideElement replenishmentButtonBefore1 = $("[data-test-id=\"92df3f1c-a033-48e6-8390-206f6b1f56c0\"] .button__text");
     private SelenideElement replenishmentButtonBefore2 = $("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"] .button__text");
 
@@ -26,33 +21,9 @@ public class DashboardPage {
     }
 
 
-    // data - класс для хранеия и извлечения информации о начальном и конечном балансе карт
-    public class CardsBalance {
-        int card1Balance;
-        int card2Balance;
-
-        public void setCard1Balance(int card1Balance) {
-            this.card1Balance = card1Balance;
-        }
-
-        public void setCard2Balance(int card2Balance) {
-            this.card2Balance = card2Balance;
-        }
-
-        public int getCard1Balance() {
-            return card1Balance;
-        }
-
-        public int getCard2Balance() {
-            return card2Balance;
-        }
-
-    }
-
     // метод получения начального баланса карт
     public int getInitialBalanceOfCard(int cardIndex) {
-        var initialCardsBalance = new CardsBalance();
-        var text = cards.get(cardIndex-1).text();
+        var text = cards.get(cardIndex - 1).text();
         return exctractBalance(text);
     }
 
@@ -65,44 +36,22 @@ public class DashboardPage {
 
 
     // метод для вычисления окончательного баланса карт
-    public CardsBalance finalBalance(String from1To2OrFrom2to1, int initialBalanceCard1, int initialBalanceCard2, int transferAmount) {
-        var сardsBalance = new CardsBalance();
+    public int finalBalance(String from1To2OrFrom2to1, int cardIndex, int initialBalanceCard1, int initialBalanceCard2, int transferAmount) {
+        int[] finalBalanceOfCard = new int[2];
         if (from1To2OrFrom2to1 == "from1To2") {
-            int finalBalanceOfTheFirstCard = initialBalanceCard1 - transferAmount;
-            int finalBalanceOfTheSecondCard = initialBalanceCard2 + transferAmount;
-            сardsBalance.setCard1Balance(finalBalanceOfTheFirstCard);
-            сardsBalance.setCard2Balance(finalBalanceOfTheSecondCard);
+            finalBalanceOfCard[0] = initialBalanceCard1 - transferAmount;
+            finalBalanceOfCard[1] = initialBalanceCard2 + transferAmount;
         }
         if (from1To2OrFrom2to1 == "from2To1") {
-            int finalBalanceOfTheFirstCard = initialBalanceCard1 + transferAmount;
-            int finalBalanceOfTheSecondCard = initialBalanceCard2 - transferAmount;
-            сardsBalance.setCard1Balance(finalBalanceOfTheFirstCard);
-            сardsBalance.setCard2Balance(finalBalanceOfTheSecondCard);
+            finalBalanceOfCard[0] = initialBalanceCard1 + transferAmount;
+            finalBalanceOfCard[1] = initialBalanceCard2 - transferAmount;
         }
-        return сardsBalance;
-    }
-
-    // метод проверки окончательного баланса
-    public int checkFinalBalance(String from1To2OrFrom2to1, int initialBalanceCard1, int initialBalanceCard2, int transferAmount) {
-        int finalBalanceOfCard1 = finalBalance(from1To2OrFrom2to1, initialBalanceCard1, initialBalanceCard2, transferAmount).getCard1Balance();
-        int finalBalanceOfCard2 = finalBalance(from1To2OrFrom2to1, initialBalanceCard1, initialBalanceCard2, transferAmount).getCard2Balance();
-        // проверка осуществления перевода
-        firstCard.shouldHave(text(String.valueOf(finalBalanceOfCard1)));
-        secondCard.shouldHave(text(String.valueOf(finalBalanceOfCard2)));
-        // возвращение баланса в тест для последующего ассерта в тесте с негативным сценарием
-        int cardBalance = 0;
-        if (from1To2OrFrom2to1 == "from1To2") {
-            cardBalance = finalBalanceOfCard1;
-        }
-        if (from1To2OrFrom2to1 == "from2To1") {
-            cardBalance = finalBalanceOfCard2;
-        }
-        return cardBalance;
+        return finalBalanceOfCard[cardIndex - 1];
     }
 
     // метод нажатия кнопки для переключения на страницу оплаты
-    public ReplenishmentPage clickReplenishmentButton (String from1To2OrFrom2to1) {
-        if (from1To2OrFrom2to1=="from1to2") {
+    public ReplenishmentPage clickReplenishmentButton(String from1To2OrFrom2to1) {
+        if (from1To2OrFrom2to1 == "from1to2") {
             replenishmentButtonBefore2.click();
         } else {
             replenishmentButtonBefore1.click();
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,16 +1,22 @@
 package ru.netology.web.test;
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.*;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static org.junit.jupiter.api.Assertions.*;
 
 import static com.codeborne.selenide.Selenide.open;
 
 class MoneyTransferTest {
+    private SelenideElement firstCard = $("[data-test-id=\"92df3f1c-a033-48e6-8390-206f6b1f56c0\"]");
+    private SelenideElement secondCard = $("[data-test-id=\"0f3f5c2a-249e-4c3d-8287-09f7a039391d\"]");
+
     @BeforeEach
         // метод сравнивает баланс карт, если он не равен, то разницу между картами отправляет на карту, где баланс меньше
     void shouldRestoreCardsBalanceIfItIsNeeded() {
@@ -54,7 +60,11 @@ class MoneyTransferTest {
         int initialBalanceCard2 = dashboardPage.getInitialBalanceOfCard(2);
         var replenishmentPage = dashboardPage.clickReplenishmentButton("from2to1");
         replenishmentPage.replenishment(secondCardNumber, amountValue);
-        dashboardPage.checkFinalBalance("from2To1", initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        int finalBalanceOfCard1 = dashboardPage.finalBalance("from2To1", 1, initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        int finalBalanceOfCard2 = dashboardPage.finalBalance("from2To1", 2, initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        // проверка осуществления перевода
+        firstCard.shouldHave(text(String.valueOf(finalBalanceOfCard1)));
+        secondCard.shouldHave(text(String.valueOf(finalBalanceOfCard2)));
     }
 
     @Test
@@ -71,7 +81,11 @@ class MoneyTransferTest {
         int initialBalanceCard2 = dashboardPage.getInitialBalanceOfCard(2);
         var replenishmentPage = dashboardPage.clickReplenishmentButton("from1to2");
         replenishmentPage.replenishment(firstCardNumber, amountValue);
-        dashboardPage.checkFinalBalance("from1To2", initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        int finalBalanceOfCard1 = dashboardPage.finalBalance("from1To2", 1, initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        int finalBalanceOfCard2 = dashboardPage.finalBalance("from1To2", 2, initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        // проверка осуществления перевода
+        firstCard.shouldHave(text(String.valueOf(finalBalanceOfCard1)));
+        secondCard.shouldHave(text(String.valueOf(finalBalanceOfCard2)));
     }
 
     @Test
@@ -88,8 +102,13 @@ class MoneyTransferTest {
         int initialBalanceCard2 = dashboardPage.getInitialBalanceOfCard(2);
         var replenishmentPage = dashboardPage.clickReplenishmentButton("from2to1");
         replenishmentPage.replenishment(secondCardNumber, amountValue);
-        int finalBalanceActual = dashboardPage.checkFinalBalance("from2To1", initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
-        assertTrue(finalBalanceActual > 0);
+        int finalBalanceOfCard1 = dashboardPage.finalBalance("from2To1", 1, initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        int finalBalanceOfCard2 = dashboardPage.finalBalance("from2To1", 2, initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        // проверка осуществления перевода
+        firstCard.shouldHave(text(String.valueOf(finalBalanceOfCard1)));
+        secondCard.shouldHave(text(String.valueOf(finalBalanceOfCard2)));
+        // проверка того, что баланс второй карты больше нуля
+        assertTrue(finalBalanceOfCard2 > 0);
     }
 
     @Test
@@ -106,8 +125,13 @@ class MoneyTransferTest {
         int initialBalanceCard2 = dashboardPage.getInitialBalanceOfCard(2);
         var replenishmentPage = dashboardPage.clickReplenishmentButton("from1to2");
         replenishmentPage.replenishment(firstCardNumber, amountValue);
-        int finalBalanceActual = dashboardPage.checkFinalBalance("from1To2", initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
-        assertTrue(finalBalanceActual > 0);
+        int finalBalanceOfCard1 = dashboardPage.finalBalance("from1To2", 1, initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        int finalBalanceOfCard2 = dashboardPage.finalBalance("from1To2", 2, initialBalanceCard1, initialBalanceCard2, Integer.parseInt(amountValue));
+        // проверка осуществления перевода
+        firstCard.shouldHave(text(String.valueOf(finalBalanceOfCard1)));
+        secondCard.shouldHave(text(String.valueOf(finalBalanceOfCard2)));
+        // проверка того, что баланс первой карты больше нуля
+        assertTrue(finalBalanceOfCard1 > 0);
     }
 
 }
